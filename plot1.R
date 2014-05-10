57c168a9d7e6e370430380d68bdcd2bc71000bd3
Philip Graff <philip.graff@ligo.org>
2014-05-09 23:52:44 -0400

    Add plot1 script and figure

---
+++ plot1.R
@@ -0,0 +1,14 @@
+library(data.table)
+
+## read in data
+DT <- read.csv("household_power_consumption.txt",na.strings=c("?"),colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"),sep=";")
+
+# select rows with required dates
+GAP <- DT$Global_active_power[DT$Date=="1/2/2007"|DT$Date=="2/2/2007"]
+
+## make plot
+hist(GAP,col="red",xlab="Global Active Power (kilowatts)",main="Global Active Power")
+
+## save to png
+dev.copy(png,file="plot1.png")
+dev.off()
